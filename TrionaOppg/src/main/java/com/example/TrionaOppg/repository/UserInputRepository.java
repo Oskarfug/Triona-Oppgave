84ea392b550da07b34f812d62c8416d9d44dd79e
Oskarfug <59930916+Oskarfug@users.noreply.github.com>
2022-07-16 00:24:37 +0200

    Delete TrionaOppg directory

--- TrionaOppg/src/main/java/com/example/TrionaOppg/repository/UserInputRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.TrionaOppg.repository;
-
-import com.example.TrionaOppg.model.UserInput;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface UserInputRepository extends CrudRepository<UserInput, Long>{
-
-}
