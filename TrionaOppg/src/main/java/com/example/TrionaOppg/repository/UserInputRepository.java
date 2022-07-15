77cf195af9410a731f039414185083b8fda595b4
Oskarfug <59930916+Oskarfug@users.noreply.github.com>
2022-07-15 20:34:02 +0200

    Add files via upload

---
+++ TrionaOppg/src/main/java/com/example/TrionaOppg/repository/UserInputRepository.java
@@ -0,0 +1,9 @@
+package com.example.TrionaOppg.repository;
+
+import com.example.TrionaOppg.model.UserInput;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface UserInputRepository extends CrudRepository<UserInput, Long>{
+
+}
