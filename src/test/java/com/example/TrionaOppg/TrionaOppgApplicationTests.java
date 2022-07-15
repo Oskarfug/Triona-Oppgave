a8cb485d7b9d1aeab4c7a738d1f028b3d567d5cd
Oskarfug <59930916+Oskarfug@users.noreply.github.com>
2022-07-16 00:25:46 +0200

    Add files via upload

---
+++ src/test/java/com/example/TrionaOppg/TrionaOppgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrionaOppg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrionaOppgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ target/classes/schema.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE user_input (
+    id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    navn VARCHAR(250),
+    adresser VARCHAR(250),
+    telefon_nummer VARCHAR(250),
+    fodselsdato VARCHAR(250),
+    rev_navn VARCHAR(250),
+    tverrsum_tlf VARCHAR(250),
+    skuddar VARCHAR(250),
+)
